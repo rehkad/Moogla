9ec8ec4284a60c7a9bddb3ec57a576436f0f1d26
rehkad <100399936+rehkad@users.noreply.github.com>
2025-06-24 10:34:09 -0700

    feat(auth): add User model

---
+++ src/moogla/auth.py
@@ -0,0 +1,9 @@
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
+class User(SQLModel, table=True):
+    """Simple user model with a unique username."""
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str = Field(index=True, sa_column_kwargs={"unique": True})
+    hashed_password: str
